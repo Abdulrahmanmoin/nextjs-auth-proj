d1f6055352a46bf2858fe6622a5e2a24cc7a4d59
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-12-26 15:47:29 +0500

    remove unnecessary logs

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -8,25 +8,12 @@ import { DB_NAME } from "../../../../../constants";
 
 export async function POST(request: NextRequest) {
     try {
-        
-        console.log("Let's start");
 
         await connectDB();
 
-
-        console.log(`"MONGO_URI: ", ${process.env.MONGO_URI!}/${DB_NAME}${process.env.MONGO_URI_PARAMETERS}`);
-
-
-        const start = Date.now();
         const { email, username, password } = await request.json()
 
-        console.log('Start database query findone-email');
-
         const user = await User.findOne({ email })
-
-        const findOneEmailQueryTime = Date.now() - start;
-        console.log(`Database query of finding by email completed in ${findOneEmailQueryTime}ms`);
-
 
         if (user) {
             return NextResponse.json({ message: "User already exists" }, { status: 400 });
@@ -34,22 +21,9 @@ export async function POST(request: NextRequest) {
 
         const encryptPassword = await bcrypt.hash(String(password), 10)
 
-        console.log('Start database query of sending email');
-        const start2 = Date.now();
-
-
         sendMail(email, "Verify your email").catch((err) => console.error("Failed to send email:", err));
 
-        console.log(`Database query of sending email: ${Date.now() - start2}ms`);
-
-
-        console.log('Start database query of creating a user');
-        const start3 = Date.now();
-
-
         const newUser = await User.create({ email, username, password: encryptPassword })
-
-        console.log(`Database query of creating a user completed in ${Date.now() - start3}ms`);
 
         return NextResponse.json({ message: "User created succesfully", newUser }, { status: 200 });
 
--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -21,7 +21,7 @@ export async function sendMail(receiverEmail: string, subject: string) {
   // send mail with defined transport object
 
   await connectDB();
-  
+
   const generateToken = () => {
     return crypto.randomBytes(32).toString("hex");
   };
@@ -34,15 +34,7 @@ export async function sendMail(receiverEmail: string, subject: string) {
   if (subject === "Verify your email") {
     emailHtmlRoute = "verifyemail";
 
-    const start = Date.now();
-
-    console.log('Start database query findone-emailand update(send mail)');
-
     const user = await User.findOneAndUpdate({ email: receiverEmail }, { verifyToken: token, verifyTokenExpiry: Date.now() + 7200000 }, { new: true })
-
-    const findOneEmailQueryTime = Date.now() - start;
-    console.log(`Database query of findone-emailand update(send mail) completed in ${findOneEmailQueryTime}ms`);
-
 
     if (!user) {
       return NextResponse.json({ message: "Check your email" }, { status: 400 });
@@ -53,15 +45,7 @@ export async function sendMail(receiverEmail: string, subject: string) {
   else if (subject === "Reset password") {
     emailHtmlRoute = "resetpassword";
 
-    const start = Date.now();
-
-    console.log('Start database query findone-emailand update(send mail)');
-
     const user = await User.findOneAndUpdate({ email: receiverEmail }, { forgotPasswordToken: token, forgotPasswordTokenExpiry: Date.now() + 7200000 }, { new: true })
-
-    const findOneEmailQueryTime = Date.now() - start;
-    console.log(`Database query of findone-emailand update(send mail) completed in ${findOneEmailQueryTime}ms`);
-
 
     if (!user) {
       return NextResponse.json({ message: "Check your email" }, { status: 400 });
@@ -69,12 +53,7 @@ export async function sendMail(receiverEmail: string, subject: string) {
 
   }
 
-
   const htmlBody = `<p>Click <a href=${process.env.DOMAIN}/${emailHtmlRoute}?token=${token}>here</a> to ${subject} or copy and paste the link below in the browser.</p> <br> <a href=${process.env.DOMAIN}/${emailHtmlRoute}?token=${token}>${token}</a>`
-
-  const start = Date.now();
-
-  console.log('Start database query info(send mail)');
 
   const info = await transporter.sendMail({
     from: 'iam.armoin@gmail.com', // sender address
@@ -83,14 +62,4 @@ export async function sendMail(receiverEmail: string, subject: string) {
     html: htmlBody,
   });
 
-
-
-  const findOneEmailQueryTime = Date.now() - start;
-  console.log(`Database query of info(send mail) completed in ${findOneEmailQueryTime}ms`);
-
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
 }
-
-// main().catch(console.error);
